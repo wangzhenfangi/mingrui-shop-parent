ee70c8b002f75f47b44f7ed8769ddaa515b36422
86158 <wangzhenfangi@163.com>
2021-01-06 09:24:35 +0800

    henhao

--- mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/servce/impl/GoodsServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/servce/impl/GoodsServiceImpl.java
@@ -3,10 +3,16 @@ package com.baidu.shop.servce.impl;
 import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpuDTO;
+import com.baidu.shop.entity.SpuEntity;
 import com.baidu.shop.mapper.SpuMapper;
 import com.baidu.shop.service.GoodsService;
+import com.baidu.shop.utils.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -27,8 +33,26 @@ public class GoodsServiceImpl extends BaseApiService implements GoodsService {
 
     @Override
     public Result<List<SpuDTO>> getSpuInfo(SpuDTO spuDTO) {
+        //分页
+        if(ObjectUtil.isNotNull(spuDTO.getPage()) && ObjectUtil.isNotNull(spuDTO.getRows()))
+            PageHelper.startPage(spuDTO.getPage(),spuDTO.getRows());
 
-        return null;
+        Example example = new Example(SpuEntity.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        //是否上架
+        if(ObjectUtil.isNotNull(spuDTO.getSaleable()) && spuDTO.getSaleable() < 2){
+            criteria.andEqualTo("saleable",spuDTO.getSaleable());
+        }
+        //模糊查询
+        if(!StringUtils.isEmpty(spuDTO.getTitle())) criteria.andLike("title","%" + spuDTO.getTitle() + "%");
+
+        List<SpuEntity> spuEntities = spuMapper.selectByExample(example);
+        PageInfo<SpuEntity>  pageInfo = new PageInfo<>(spuEntities);
+
+        return this.setResultSuccess(pageInfo);
+
+
     }
 
 
