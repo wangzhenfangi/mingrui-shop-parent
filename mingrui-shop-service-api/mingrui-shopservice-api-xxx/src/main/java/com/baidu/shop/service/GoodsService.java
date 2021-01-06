0f0bc06f9efb9352f759c38ff53db3cc2049637c
86158 <wangzhenfangi@163.com>
2021-01-06 09:28:06 +0800

    henhao

--- mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/GoodsService.java
+++ mingrui-shop-service-api/mingrui-shopservice-api-xxx/src/main/java/com/baidu/shop/service/GoodsService.java
@@ -2,6 +2,8 @@ package com.baidu.shop.service;
 
 import com.baidu.shop.base.Result;
 import com.baidu.shop.dto.SpuDTO;
+import com.baidu.shop.entity.SpuEntity;
+import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,6 @@ import java.util.List;
 public interface GoodsService {
     @ApiOperation(value = "获取spu信息")
     @GetMapping(value = "goods/getSpuInfo")
-    public Result<List<SpuDTO>> getSpuInfo(SpuDTO spuDTO);
+    public Result<PageInfo<SpuEntity>> getSpuInfo(SpuDTO spuDTO);
 
 }
--- mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/servce/impl/GoodsServiceImpl.java
+++ mingrui-shop-service/mingrui-shop-servicexxx/src/main/java/com/baidu/shop/servce/impl/GoodsServiceImpl.java
@@ -32,7 +32,7 @@ public class GoodsServiceImpl extends BaseApiService implements GoodsService {
     private SpuMapper spuMapper;
 
     @Override
-    public Result<List<SpuDTO>> getSpuInfo(SpuDTO spuDTO) {
+    public Result<PageInfo<SpuEntity>> getSpuInfo(SpuDTO spuDTO) {
         //分页
         if(ObjectUtil.isNotNull(spuDTO.getPage()) && ObjectUtil.isNotNull(spuDTO.getRows()))
             PageHelper.startPage(spuDTO.getPage(),spuDTO.getRows());
